cc2a820fe19958e37f8d3484fc0d84891b7411d1
Filipe Oliveira <filipe.ocosta@usp.br>
2021-06-19 02:41:22 -0300

    Organizando rotinas do codigo de maneira logica

--- conversor.asm
+++ conversor.asm
@@ -101,6 +101,143 @@ entradaDec:
 	
 	j StrtoDec			#jump to the routine that will turn the string into a int type decimal
 
+#################################################
+# Converters from input strings to int variable #
+#################################################
+
+HextoDec:
+	la $s1, nro_hexa		#moving to $s1 the address of nro_hexa string
+	li $s2, 0			#initializing register $s2 which will store the length of the string
+	li $s4, 8			#storing in $s4 the maximum length of the string
+	li $s3, '\0'
+	li $t9, '\n'			#$s3 and $t9 will be used to find the end of string
+	li $s5, 1			#$s5 holds the value of the powers of 16, starting with 16^0 and increasing the exponent with each execution
+	li $s7, 16			#multiplies the $s5 incresing the exponent
+	li $s6, 0			#used as an adder
+	lb $t7, 0($s1)			#$t7 will store the value of each char in the string
+
+	achaTamanho:				#this function traverses the string to find the length
+		beq $s3, $t7, testeOverflow 	#breaks loop if end of string is found
+		beq $t9, $t7, testeOverflow
+		addi $s1, $s1, 1		#when the loop ends, it points to the last element of the string
+		addi $s2, $s2, 1		#store the length of string
+		lb $t7, 0($s1)
+		j achaTamanho
+	
+	testeOverflow:
+		bgt $s2, $s4, error		#tests if there is an overflow by comparing the length of string
+	converterHD:
+		sw $s6, numeroDecimal		#store the final sum value in numeroDecimal
+		beq $s2, $zero, baseSaida	#ends the loop when the entire string has been traversed, going to baseSaida
+	
+		addi $s1, $s1, -1		#move to $t7 the value of next char
+		lb $t7, 0($s1)
+
+		sub $t7, $t7, 48		#sequency of tests to extract character values
+		blt $t7, $zero, error 		#verify possible errors and ends the program if finds one
+		li $s4, 9
+		ble $t7, $s4  multiPot 
+		sub $t7, $t7, 7
+		ble $t7, $s4, error
+		li $s4, 15
+		ble $t7, $s4, multiPot
+		sub $t7, $t7, 32
+		bgt $t7, $s4, error
+		li $s4, 10
+		blt $t7, $s4, error
+
+	multiPot:
+		mul $t5, $s5, $t7		#multiplying the power of 16 by the value of the char and storing it in $t5
+		add $s6, $s6, $t5		#store the sum value in $s6
+		mul $s5, $s5, $s7		#increasing the power of 16 exponent
+		addi $s2, $s2, -1		#decreasing the current position
+		j converterHD
+
+
+BintoDec:
+	la $s1, nro_bin
+	li $s2, 0			#initializing register $s2 which will store the length of the string
+	li $t1, 32			#storing in $t1 the maximum length of the string
+	li $s3, '\0'
+	li $t9, '\n'
+	li $s4, '0'
+	li $t4, '1'			#$s4 and $t4 will be used to compare the value of char
+	li $s5, 1			#$s5 holds the value of the powers of 2, starting with 2^0 and increasing the exponent with each execution
+	li $s7, 2			#multiplies the $s5 incresing the exponent
+	li $s6, 0
+	lb $t7, 0($s1)
+
+	findLength:				
+		beq $s3, $t7, testeOver 
+		beq $t9, $t7, testeOver
+		addi $s1, $s1, 1
+		addi $s2, $s2, 1
+		lb $t7, 0($s1)
+		j findLength
+	
+	testeOver:
+		bgt $s2, $t1, error		#tests if there is an overflow by comparing the length of string
+
+	converterBD:
+		sw $s6, numeroDecimal		#store the final sum value in numeroDecimal
+		beq $s2, $zero, baseSaida
+
+		addi $s1, $s1, -1
+		lb $t7, 0($s1)
+		
+		beq $s4, $t7, continue		#if char equal to '0' goes to "continue"
+		bne $t4, $t7, error			#if char different of '1' goes to error
+		add $s6, $s6, $s5			#adding the power of 2 to the sum if char equal to '1'
+		continue:
+			mul $s5, $s5, $s7		#increasing the power of 2 exponent
+			addi $s2, $s2, -1		
+			j converterBD
+
+
+StrtoDec:
+	la $s1, nro_dec
+	la $t1, str_dec
+	li $s2, 0
+	li $s3, '\0'
+	li $t9, '\n'
+	li $s5, 1
+	li $s7, 10
+	li $s6, 0
+	lb $t7, 0($s1)
+	lb $t8, 0($t1)
+	
+	achaLength:
+		beq $s3, $t7, testeO
+		beq $t9, $t7, testeO
+		addi $s1, $s1, 1
+		addi $s2, $s2, 1
+		lb $t7, 0($s1)
+		j achaLength
+
+	
+	testeO:
+		bgt $s2, $s7, error		#tests if there is an overflow by comparing the length of string
+		bne $s2, $s7, converterSD		#if the length of string was less than 10 goes to converterSD
+		sub $s1, $s1, $s2		#other way it tests if the number is equal or greater than 2^32
+		lb $t7, 0($s1)
+		bgt $t7, $t8, error
+		add $s1, $s1, $s2
+		lb $t7, 0($s1)
+
+	converterSD:
+		sw $s6, numeroDecimal
+		beq $s2, $zero, baseSaida
+		addi $s1, $s1, -1
+		lb $t7, 0($s1)
+		addi $t7, $t7, -48
+		bge $t7, $s7, error
+		blt $t7, $zero, error
+		mul $t9, $t7, $s5
+		add $s6, $s6, $t9
+		mul $s5, $s5, $s7
+		addi $s2, $s2, -1
+		j converterSD
+
 #############################################
 # Routine to read the output numerical base #
 #############################################
@@ -137,225 +274,8 @@ sentToConverter:
 	j error				#if $t0 is not equal to any of the number bases, the program goes to "error"
 
 ########################################################
-# Routines to print the output number after conversion #
+# Converters from int to correct output numerical base #
 ########################################################
-
-printDec:
-	#printando uma string
-	li $v0, 4 
-	la $a0, str_final
-	syscall				#printing string str_final
-
-	li $v0, 36
-	lw $a0, numeroDecimal
-	syscall				#printing the variable numeroDecimal as an unsigned int
-
-	j end				#ends the program
-
-printHex:
-	li $v0, 4 
-	la $a0, str_final
-	syscall				#printing string str_final
-
-	li $v0, 4
-	move $a0, $t7			#printing the string that contains the hex number
-	syscall
-
-	j end				#ends the program
-
-printBin:
-	li $v0, 4 
-	la $a0, str_final
-	syscall				#printing string str_final
-
-	li $v0, 4
-	move $a0, $t7			#printing the string that contains the binary number
-	syscall
-
-	j end				#ends the program
-
-#################################################
-# Converters from input strings to int variable #
-#################################################
-
-HextoDec:
-	la $s1, nro_hexa		#moving to $s1 the address of nro_hexa string
-	li $s2, 0			#initializing register $s2 which will store the length of the string
-	li $s4, 8			#storing in $s4 the maximum length of the string
-	li $s3, '\0'
-	li $t9, '\n'			#$s3 and $t9 will be used to find the end of string
-	li $s5, 1			#$s5 holds the value of the powers of 16, starting with 16^0 and increasing the exponent with each execution
-	li $s7, 16			#multiplies the $s5 incresing the exponent
-	li $s6, 0			#used as an adder
-	lb $t7, 0($s1)			#$t7 will store the value of each char in the string
-
-	achaTamanho:				#this function traverses the string to find the length
-		beq $s3, $t7, testeOverflow 	#breaks loop if end of string is found
-		beq $t9, $t7, testeOverflow
-		addi $s1, $s1, 1		#when the loop ends, it points to the last element of the string
-		addi $s2, $s2, 1		#store the length of string
-		lb $t7, 0($s1)
-		j achaTamanho
-	
-	testeOverflow:
-		bgt $s2, $s4, error		#tests if there is an overflow by comparing the length of string
-	converterHD:
-		sw $s6, numeroDecimal		#store the final sum value in numeroDecimal
-		beq $s2, $zero, baseSaida	#ends the loop when the entire string has been traversed
-	
-		addi $s1, $s1, -1		#move to $t7 the value of next char
-		lb $t7, 0($s1)
-
-		sub $t7, $t7, 48		#sequency of tests to extract character values
-		blt $t7, $zero, error 		#verify possible errors and ends the program if finds one
-		li $s4, 9
-		ble $t7, $s4  multiPot 
-		sub $t7, $t7, 7
-		ble $t7, $s4, error
-		li $s4, 15
-		ble $t7, $s4, multiPot
-		sub $t7, $t7, 32
-		bgt $t7, $s4, error
-		li $s4, 10
-		blt $t7, $s4, error
-
-	multiPot:
-		mul $t5, $s5, $t7		#multiplying the power of 16 by the value of the char and storing it in $t5
-		add $s6, $s6, $t5		#store the sum value in $s6
-		mul $s5, $s5, $s7		#increasing the power of 16 exponent
-		addi $s2, $s2, -1		#decreasing the current position
-		j converterHD
-
-	
-
-BintoDec:
-
-	#carregando o endereco da string com o nro binario para s1
-	la $s1, nro_bin
-
-	#s2 sera utilizado para armazenar o tamanho da string
-	li $s2, 0
-
-	#s3 e s9 utilizados para identificar o fim da string e desviar a funcao
-	li $s3, '\0'
-	li $t9, '\n'
-	
-	#s4 e t4 utilizados para verificar cada bit individualmente e transformar para valor decimal
-	li $s4, '0'
-	li $t4, '1'
-	
-	#s5 guarda o valor das potencias de 2, subindo a cada execucao
-	li $s5, 1
-
-	#utilizado para multiplicar juntamente com s5
-	li $s7, 2
-
-	#utilizado como somador
-	li $s6, 0
-
-	#t7 vai guardar o valor de cada bit da string
-	lb $t7, 0($s1)
-	
-	li $t1, 32
-
-	#laco para definir o tamanho da string
-	findLength:
-
-		#comparacoes
-		beq $s3, $t7, testeOver
-		beq $t9, $t7, testeOver
-		addi $s1, $s1, 1
-		addi $s2, $s2, 1
-		lb $t7, 0($s1)
-		j findLength
-	
-	
-	testeOver:
-		bgt $s2, $t1, error
-	#esse laco percorre a string a partir do bit menos significativo, incrementando s6 caso o bit = 1
-	#laco responsavel por verificar se o numero eh realmente binario
-	converterBD:
-		sw $s6, numeroDecimal
-		beq $s2, $zero, baseSaida
-		addi $s1, $s1, -1
-		lb $t7, 0($s1)
-		beq $s4, $t7, continue
-		bne $t4, $t7, error
-		add $s6, $s6, $s5
-		continue:
-			mul $s5, $s5, $s7
-			addi $s2, $s2, -1
-			j converterBD
-
-
-StrtoDec:
-
-	#carregando o endereco da string com o nro binario para s1
-	la $s1, nro_dec
-	la $t1, str_dec
-
-	#s2 sera utilizado para armazenar o tamanho da string
-	li $s2, 0
-
-	#s3 e s9 utilizados para identificar o fim da string e desviar a funcao
-	li $s3, '\0'
-	li $t9, '\n'
-	
-	#s5 guarda o valor das potencias de 2, subindo a cada execucao
-	li $s5, 1
-
-	#utilizado para multiplicar juntamente com s5
-	li $s7, 10
-
-	#utilizado como somador
-	li $s6, 0
-
-	#t7 vai guardar o valor de cada bit da string
-	lb $t7, 0($s1)
-	lb $t8, 0($t1)
-	
-
-	#laco para definir o tamanho da string
-	achaLength:
-
-		#percorre a string, guarda o tamanho dela em $s2 e posiciona o $t7 na ultima posicao
-		beq $s3, $t7, testeO
-		beq $t9, $t7, testeO
-		addi $s1, $s1, 1
-		addi $s2, $s2, 1
-		lb $t7, 0($s1)
-		j achaLength
-
-	
-	testeO:
-		bgt $s2, $s7, error
-		bne $s2, $s7, converterSD
-		sub $s1, $s1, $s2
-		lb $t7, 0($s1)
-		bgt $t7, $t8, error
-		add $s1, $s1, $s2
-		lb $t7, 0($s1)
-
-
-	#esse laco percorre a string a partir do bit menos significativo, incrementando s6 caso o bit = 1
-	#laco responsavel por verificar se o numero eh realmente binario
-	converterSD:
-		sw $s6, numeroDecimal
-		beq $s2, $zero, baseSaida
-		addi $s1, $s1, -1
-		lb $t7, 0($s1)
-		addi $t7, $t7, -48
-		bge $t7, $s7, error
-		blt $t7, $zero, error
-		mul $t9, $t7, $s5
-		add $s6, $s6, $t9
-		mul $s5, $s5, $s7
-		addi $s2, $s2, -1
-		j converterSD
-
-#######################################################
-# Converters from int to correct numerical base output #
-#######################################################
 
 DectoHex:
 	
@@ -441,4 +361,40 @@ DectoBin:
 	ret:
 		j printBin
 
+########################################################
+# Routines to print the output number after conversion #
+########################################################
 
+printDec:
+	#printando uma string
+	li $v0, 4 
+	la $a0, str_final
+	syscall				#printing string str_final
+
+	li $v0, 36
+	lw $a0, numeroDecimal
+	syscall				#printing the variable numeroDecimal as an unsigned int
+
+	j end				#ends the program
+
+printHex:
+	li $v0, 4 
+	la $a0, str_final
+	syscall				#printing string str_final
+
+	li $v0, 4
+	move $a0, $t7			#printing the string that contains the hex number
+	syscall
+
+	j end				#ends the program
+
+printBin:
+	li $v0, 4 
+	la $a0, str_final
+	syscall				#printing string str_final
+
+	li $v0, 4
+	move $a0, $t7			#printing the string that contains the binary number
+	syscall
+
+	j end				#ends the program
